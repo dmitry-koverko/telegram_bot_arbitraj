a6a2807582172bf17badfa10f40da889ec2e0fbe
Dmitry Koverko <dmitry.koverko@gmail.com>
2021-07-29 00:29:49 +0300

    v2

---
+++ index.js
@@ -0,0 +1,57 @@
+const { Telegraf } = require('telegraf')
+const {MenuTemplate, MenuMiddleware} = require('telegraf-inline-menu')
+const bot = new Telegraf('1720769276:AAHWkCEeeg6z5luxux3kNtMTWgcQq7GsqF4')
+
+const { Keyboard, Key } = require('telegram-keyboard')
+
+bot.command('start', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id,
+      'Добро пожаловать в бот, вот что он умеет: \n'+
+      '/menu - меню приложения', {
+    })
+})
+
+
+bot.command('menu', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'Меню', {
+
+      reply_markup: {
+                  resize_keyboard : true,
+                  keyboard: [
+
+                      [{
+                              text: "💸 Прилы",
+                              callback_data: 'balance'
+                          },
+                          {
+                              text: "📈 Цены",
+                              callback_data: 'stat'
+                          }
+                      ],
+                      [{
+                              text: "💸 Баланс",
+                              callback_data: 'balance'
+                          },
+                          {
+                              text: "📈 Статистика",
+                              callback_data: 'stat'
+                          },
+                          {
+                              text: "📋 FAQ",
+                              callback_data: 'faq'
+                          }
+                      ],
+
+                  ]
+              }
+
+    })
+})
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
